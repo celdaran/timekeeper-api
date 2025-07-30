b8336eb82021086cde874670de929773346d33ad
Charlie Hills <github.1337@tmx.email>
2025-07-30 16:06:07 -0500

    Add Nelmio Api Documentation
    This is initial support for the API documentation. At this point, the
    framework is installed and the documentation can be generated at the
    following endpoints:
    
    * http://localhost:8000/api/doc
    * http://localhost:8000/api/doc.json
    * http://localhost:8000/api/doc.yaml
    
    However, no custom annotations have been added, so it's pretty bare.
    Those will be coming next...

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['dev' => true],
 ];
